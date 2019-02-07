25abf091d8efd3722e989b202857e6af280086ca
Alexander Linné <21100826+lajne@users.noreply.github.com>
2019-02-07 09:15:34 +0100

    sound explosion added to collapse

--- Jetfighter/game.cpp
+++ Jetfighter/game.cpp
@@ -88,6 +88,7 @@ void Game::gameUpdate() {
             }
             //GAME OVER
             if(isEnemyCollidingWithPlayer(activeEnemies[i])) {
+                sound->soundExplosion();
                 health->setZero();
                 gameText->gameOver();
                 gameTimer->stop();
